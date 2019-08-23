43faf499a012e0b6c82276806baa7f69120c44ef
haoyong <hooy@haiziwang>
2019-08-23 17:12:18 +0800

    install package

--- proj/my-weixin/src/index.js
+++ proj/my-weixin/src/index.js
@@ -9,4 +9,21 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister(); // not allowed PWA(渐进式web应用程序)
+
+
+/*
+  todo
+
+  react-router doc
+  https://react-guide.github.io/react-router-cn/docs/guides/basics/Histories.html
+
+  withRouter
+  https://www.cnblogs.com/luowenshuai/p/9526341.html
+
+  wx-sdk
+  scriptjs('https://res.wx.qq.com/open/js/jweixin-1.4.0.js', () => ())
+
+  wx-sdk doc
+  https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1421141115
+*/
